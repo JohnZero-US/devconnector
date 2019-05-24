d3889689b528476a6fd4996a2a11649415a3d731
刘奇隆 <1134250142@qq.com>
2019-05-24 21:17:55 +0800

    提交

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,10 +8,31 @@
 const express = require('express');
 
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
 
-//@route    GET api/profile
-//@desc     Test route
-//@access   Public
-router.get('/', (req, res) => res.send('Profile route'));
+//@route    GET api/profile/me
+//@desc     Get current users profile
+//@access   Private
+router.get('/', auth, async (req, res) => {
+  try {
+    /* 根据User的id从数据库中找到该用户的简历 */
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate('user', ['name', 'avatar']);
+    //如果不简历存在
+    if (!profile) {
+      //返回400和信息
+      return res.status(400).json({
+        msg: 'There is no profile for this user'
+      });
+    }
+    //返回简历对象
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
