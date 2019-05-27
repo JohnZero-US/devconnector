a79a6b104d64ae70e143277ecb9d3748e11e80de
刘奇隆 <1134250142@qq.com>
2019-05-27 21:45:19 +0800

    提交

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,10 +3,61 @@ Date:2019年5月27日
 Time:15点33分
 Auth:John Zero
 */
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const { email, password } = formData;
+
+  /* 输入框的值改变的事件绑定 */
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  /* 提交事件 */
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log("SUCCESS");
+  };
+
+  return (
+    <Fragment>
+      {/* <div className="alert alert-danger">Invalid credentials</div> */}
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Sign into Your Account
+      </p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="register">Sign Up</Link>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,10 +3,12 @@ Date:2019年5月27日
 Time:15点33分
 Auth:John Zero
 */
-import React, { Fragment, userState } from "react";
+import React, { Fragment, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  const [formData, setFormData] = userState({
+  const [formData, setFormData] = useState({
     name: "",
     email: "",
     password: "",
@@ -15,8 +17,41 @@ const Register = () => {
 
   const { name, email, password, password2 } = formData;
 
+  /* 输入框的值改变的事件绑定 */
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  /* 提交事件 */
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (password !== password2) {
+      //如果两次密码不相同
+      console.log("Passwords do not match");
+    } else {
+      //请求配置
+      const config = {
+        header: {
+          "Content-Type": "application/json"
+        }
+      };
+      //新建一个用户对象
+      const newUser = {
+        name,
+        email,
+        password
+      };
+      //
+      try {
+        //发送post请求，并接收响应对象
+        const res = await axios.post("/api/users", newUser, config);
+        //打印响应数据
+        console.log(res.data);
+      } catch (error) {
+        //打印错误信息
+        console.error(error.response.data);
+      }
+    }
+  };
 
   return (
     <Fragment>
@@ -24,7 +59,7 @@ const Register = () => {
       <p className="lead">
         <i className="fas fa-user" /> Create Your Account
       </p>
-      <form className="form" action="create-profile.html">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <input
             type="text"
@@ -36,7 +71,14 @@ const Register = () => {
           />
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" />
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
           <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email
@@ -48,6 +90,9 @@ const Register = () => {
             placeholder="Password"
             name="password"
             minLength="6"
+            value={password}
+            onChange={e => onChange(e)}
+            required
           />
         </div>
         <div className="form-group">
@@ -56,12 +101,15 @@ const Register = () => {
             placeholder="Confirm Password"
             name="password2"
             minLength="6"
+            value={password2}
+            onChange={e => onChange(e)}
+            required
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="login">Sign In</Link>
       </p>
     </Fragment>
   );
